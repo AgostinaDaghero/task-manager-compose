915f60e8b05c9c5c4085ed38560f6c051b528d7b
AgostinaDaghero <ago.daghero@gmail.com>
2025-08-11 22:25:04 +0200

    Setup MainActivity with TaskScreen and ViewModel integration

--- app/src/main/java/com/adaghero/mytasks/presentation/MainActivity.kt
+++ app/src/main/java/com/adaghero/mytasks/presentation/MainActivity.kt
@@ -7,11 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.adaghero.mytasks.ui.theme.MyTasksTheme
+import com.adaghero.mytasks.viewmodel.TaskViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyTasksTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface (
+                        modifier = Modifier.padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
+                    ){
+                        val taskViewModel: TaskViewModel = viewModel()
+                        TaskScreen(taskViewModel = taskViewModel)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyTasksTheme {
-        Greeting("Android")
     }
 }
